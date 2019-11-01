76975f4a245eaa8ead0a236464d993ccb09cb51d
varun508 <imva508@gmail.com>
2019-11-01 19:24:06 +0530

    add simple view and databinding

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 export class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [1, 2, 3, 4, 5]
+    }
+  }
+
+  handleClick(e) {
+    const items = [this.state.items, 6];
+    this.setState({ items })
+  }
+
   render() {
     return (
-      <div>hello</div>
+      <div>{this.state.items.map(num => (<h3 key={num}>{num}</h3>))}
+        <button onClick={this.handleClick.bind(this)} >click</button>
+      </div>
     );
   }
 }
